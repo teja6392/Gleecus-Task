d1c665a95ff07387117c18300a1df0ee4183a81e
teja6392 <tejastudent777@gmail.com>
2022-06-22 21:21:47 +0530

    EveryThingDone wthout Resposnive

--- graphui/src/Components/Header.jsx
+++ graphui/src/Components/Header.jsx
@@ -5,12 +5,17 @@ import Badge from '@mui/material/Badge'
 import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import InputLabel from '@mui/material/InputLabel';
-import { CardContent, Grid, NativeSelect, Typography } from '@mui/material';
+import { CardContent, FormControl, Grid, MenuItem, NativeSelect, Select, Typography } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { Box } from '@mui/system';
 import { useEffect } from 'react';
 import axios from "axios"
 import Plot from "react-plotly.js";
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import TextField from '@mui/material/TextField'
 
 const Header = () => {
     let [APIdata, setData] = useState(null)
@@ -79,62 +84,170 @@ const Header = () => {
             </div>
 
             {/* Second Part */}
-            <Grid container m={3}>
+            <Grid container mt={2} mb={2}>
                 <Typography variant='h6' sx={{ margin: "0px 20px" }}>Admin</Typography>
                 <ArrowForwardIcon color="action" fontSize='large' />
                 <Typography variant="h5" sx={{ marginLeft: "20px", color: "blue" }}>PendingOrders</Typography>
             </Grid>
-            <Grid container ml={3}>
+            <Grid container>
                 <Typography variant="h3">Pending Orders</Typography>
             </Grid>
 
 
 
             {/* Third part Cards*/}
-            <Grid container m={2}>
+            <Grid container m={1}>
                 {/* Let's Intialize x and y values before developing graph */}
-                {X && Y.map((x) => {
-                    console.log(x)
-                })}
-                {APIdata ? <Plot
-                    id="plot" style={{ width: "48%" }}
-                    data={[{ x: X, y: Y, type: "bar", orientation: 'h', autosize: "false" }]}
-                    layout={{
-                        width: 700, height: 800, title: "Histogram",
-                        margin: { l: 150 }
-                    }}
-
-                /> : ""}
-                {/* <Typography variant="h5" component="div">
-                        benevolent
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        adjective
-                    </Typography>
-                    <Typography variant="body2">
-                        well meaning and kindly.
-                        <br />
-                        {'"a benevolent smile"'}
-                    </Typography> */}
-
 
                 <CardContent
-                    sx={{ border: "1px solid white", width: "48%", marginRight: "10px", backgroundColor: "black", marginLeft: "10px" }}
+                    sx={{ border: "1px solid white", width: "48%", backgroundColor: "snow" }} m={1}
                 >
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        benevolent
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        adjective
-                    </Typography>
-                    <Typography variant="body2">
-                        well meaning and kindly.
-                        <br />
-                        {'"a benevolent smile"'}
-                    </Typography>
+                    {APIdata ?
+                        <Grid>
+                            <Grid sx={{ float: 'right', padding: "5px" }}>
+                                <RemoveRedEyeIcon color="action" fontSize='large' />
+                                <VisibilityOffIcon color="action" fontSize='large' />
+                                <ChangeCircleIcon color="action" fontSize='large' />
+                                <span style={{ fontWeight: "bold" }}><button>HIstogram</button></span>
+                            </Grid>
+                            <Plot
+                                id="plot"
+                                data={[{ x: X, y: Y, type: "bar", orientation: 'h', autosize: "false" }]}
+                                layout={{
+                                    width: 600, height: 900,
+                                    margin: { l: 104, r: 0, t: 0, b: 0 }
+                                }}
+
+                            />
+                        </Grid> : ""}</CardContent>
+
+
+                {/* Second Card */}
+                <CardContent 
+                    sx={{ border: "1px solid white", position:"relative",width: "48%", backgroundColor: "snow",marginLeft:"5px"
+                    }}>
+
+                   <Grid mt={10}>
+                     {/* Card2 First PArt Date And Time */}
+                     <Grid sx={{ margin: "auto" }} m={3}>
+                        <TextField id="date" label="StartDate" type="date" InputLabelProps={{ shrink: true }}
+                            sx={{ width: "49%", backgroundColor: "aliceblue", marginRight: "2px" }} />
+                        <TextField id="date" label="EndDate" type="date" InputLabelProps={{ shrink: true }}
+                            sx={{ width: "49%", backgroundColor: "aliceblue" }} />
+                    </Grid>
+
+                    {/* card2 SEcond part */}
+                    <Grid container>
+                        <Grid m={3}>
+                            <Typography variant='h4'>Pending Orders</Typography>
+                            <Typography variant='h5'>90</Typography>
+                        </Grid>
+                        <Grid m={3}>
+                            <Typography variant='h4'>Revenue On Hold</Typography>
+                            <Typography variant='h5'>$3000</Typography>
+                        </Grid>
+                    </Grid>
+
+                    {/* card 2 Third Part */}
+                    <Grid container m={1}>
+                        <Grid sx={{ border: "1px solid black" }} m={1}>
+                            <Typography variant='h5'>In House Preprocessing</Typography>
+                            <hr />
+                            <Grid container mt={1}>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Orders</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Episodes</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Revenue</Typography>
+                                    <Typography variant='body'>$1000</Typography>
+                                </Grid>
+                            </Grid>
+                            <hr />
+                            <Grid>
+                                <a style={{ color: "blue" }}>View Details</a>
+                            </Grid>
+                        </Grid>
+
+                        <Grid sx={{ border: "1px solid black" }} m={1}>
+                            <Typography variant='h5'>Pending Signatures</Typography>
+                            <hr />
+                            <Grid container mt={1}>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Orders</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Episodes</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Revenue</Typography>
+                                    <Typography variant='body'>$1000</Typography>
+                                </Grid>
+                            </Grid>
+                            <hr />
+                            <Grid>
+                                <a style={{ color: "blue" }}>View Details</a>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+
+                    {/* card2 Fourth Part */}
+                    <Grid container m={1}>
+                        <Grid sx={{ border: "1px solid black" }} m={1}>
+                            <Typography variant='h5'>In House Preprocessing</Typography>
+                            <hr />
+                            <Grid container mt={1}>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Orders</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Episodes</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Revenue</Typography>
+                                    <Typography variant='body'>$1000</Typography>
+                                </Grid>
+                            </Grid>
+                            <hr />
+                            <Grid>
+                                <a style={{ color: "blue" }}>View Details</a>
+                            </Grid>
+                        </Grid>
+
+                        <Grid sx={{ border: "1px solid black" }} m={1}>
+                            <Typography variant='h5'>Pending Signatures</Typography>
+                            <hr />
+                            <Grid container mt={1}>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Orders</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Episodes</Typography>
+                                    <Typography variant='body'>10</Typography>
+                                </Grid>
+                                <Grid m={1}>
+                                    <Typography variant='h6' >Revenue</Typography>
+                                    <Typography variant='body'>$1000</Typography>
+                                </Grid>
+                            </Grid>
+                            <hr />
+                            <Grid>
+                                <a style={{ color: "blue" }}>View Details</a>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                   </Grid>
+
                 </CardContent>
             </Grid>
 
